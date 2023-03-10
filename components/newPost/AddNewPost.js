62656124c120994285667f55ca8fa8180c14d800
Shafique Ahmed <shafiqued17z@gmail.com>
2023-03-10 23:43:36 +0500

    New header updates for posting

--- components/newPost/AddNewPost.js
+++ components/newPost/AddNewPost.js
@@ -1,46 +1,63 @@
 import React from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+// import { Divider } from "react-native-elements/dist/divider/Divider";
 
 const AddNewPost = () => (
   <View style={styles.container}>
     <Header />
+    {/* <Formik */}
   </View>
 );
 
 const Header = () => (
-  <View style={styles.headerContainer}>
-    <TouchableOpacity>
+  <View>
+    <View style={styles.headerContainer}>
+      <TouchableOpacity>
+        <Image
+          source={{
+            uri: "https://img.icons8.com/material-rounded/60/ffffff/delete-sign.png",
+          }}
+          style={{
+            width: 30,
+            height: 30,
+          }}
+        />
+      </TouchableOpacity>
+      <Text style={styles.headerText}>New post</Text>
+      <Text></Text>
       <Image
         source={{
-          uri: "https://img.icons8.com/ios-glyphs/90/ffffff/back.png",
+          uri: "https://img.icons8.com/fluency-systems-regular/96/0396ef/long-arrow-right.png",
         }}
         style={{
           width: 30,
           height: 30,
         }}
       />
-    </TouchableOpacity>
-    <Text style={styles.headerText}>NEW POST</Text>
-    <Text></Text>
+    </View>
+    {/* <Divider width={1} color="#f7f7f7" orientation="horizontal" /> */}
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 10,
+    // marginHorizontal: 1,
   },
   headerContainer: {
-    marginTop: 5,
+    // backgroundColor: "#5e64e5",
+    // marginTop: 5,
+    padding: 12,
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "flex-start",
     alignItems: "center",
   },
   headerText: {
     color: "white",
-    marginTop: 5,
     fontSize: 20,
+    marginStart: 25,
+    marginEnd: 170, //hardcoded the right arrow icon to the end of screen.
     fontWeight: "700",
-    marginRight: 20,
+    // marginRight: 20,
   },
 });
 
